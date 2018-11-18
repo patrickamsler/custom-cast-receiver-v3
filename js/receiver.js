0e6af1d03667c5092efe9e20c1c05689d89ae750
Patrick Amsler <patrick.amsler@zuhlke.com>
2018-11-18 01:17:04 -0800

    test code for license url and message interception added

--- js/receiver.js
+++ js/receiver.js
@@ -1,23 +1,89 @@
 'use strict';
 const logEvents = false; // set to false in production due to performance issues
+const debug = true;
+const licenseUrl = 'http://path/to/license/server';
 
 const context = cast.framework.CastReceiverContext.getInstance();
 const playerManager = context.getPlayerManager();
-const playbackConfig = playerManager.getPlaybackConfig();
 
-if (logEvents) {
+
+const init = () => {
+    initPlaybackConfig();
+
+    if (logEvents) {
+        addEventLogging()
+    }
+    const options = new cast.framework.CastReceiverOptions();
+    addReceiverOptions(options);
+    if (debug) {
+        addReceiverDebugOptions(options)
+    }
+
+    addLicenseUrl(licenseUrl); // TODO test with license server
+    addMessageInterceptor();
+
+    context.start(options);
+};
+
+const initPlaybackConfig = () => {
+    const playbackConfig = (Object.assign(new cast.framework.PlaybackConfig(), playerManager.getPlaybackConfig()));
+
+    playbackConfig.autoResumeDuration = 5; // Set the player to start playback as soon as there are five seconds of media content buffered. Default is 10.
+    playbackConfig.autoResumeNumberOfSegments = 1;
+
+    playerManager.setPlaybackConfig(playbackConfig);
+};
+
+const addReceiverOptions = (options) => {
+    // TODO
+};
+
+const addReceiverDebugOptions = (options) => {
+    console.log('initialize debug options');
+    options.maxInactivity = 3600; //Development only
+};
+
+const addEventLogging = () => {
+    console.log('add event logging');
     // Listen and log all Core Events
     playerManager.addEventListener(
         cast.framework.events.category.CORE,
         event => {
             console.log(event);
         });
-}
+};
 
-if (playbackConfig) {
-    // Set the player to start playback as soon as there are five seconds of
-    // media content buffered. Default is 10.
-    playbackConfig.autoResumeDuration = 5;
-}
+const addLicenseUrl = (licenseUrl) => {
+    console.log(`add license url ${licenseUrl}`);
 
-context.start();
+    // Customize the license url for playback
+    const playbackConfig = playerManager.getPlaybackConfig();
+    playbackConfig.licenseUrl = licenseUrl;
+    playbackConfig.licenseRequestHandler = requestInfo => {
+        requestInfo.withCredentials = true;
+    };
+    playerManager.setPlaybackConfig(playbackConfig);
+
+    // Update playback config licenseUrl according to provided value in load request.
+    playerManager.setMediaPlaybackInfoHandler((loadRequest, playbackConfig) => {
+        if (loadRequest.media.customData && loadRequest.media.customData.licenseUrl) {
+            playbackConfig.licenseUrl = loadRequest.media.customData.licenseUrl;
+        }
+        return playbackConfig;
+    });
+};
+
+const addMessageInterceptor = () => {
+    playerManager.setMessageInterceptor(
+        cast.framework.messages.MessageType.LOAD,
+        request => {
+            // Request can be modified here. It is also possible to fetch additional data from a third
+            // party server before continuing with the request.
+            // see https://developers.google.com/cast/docs/caf_receiver/advanced#message-interception
+            console.log(JSON.stringify(request));
+            return request;
+        });
+};
+
+// start cast framework
+init();
